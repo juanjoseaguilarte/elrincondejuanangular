e50a167ed926f12947dc42074635616a50683519
Juan Jose Aguilarte <barelrincon@gmail.com>
2022-08-07 23:56:54 +0200

    Add view alergic and language

--- src/app/components/carta/carta.component.ts
+++ src/app/components/carta/carta.component.ts
@@ -13,6 +13,7 @@ export class CartaComponent implements OnInit {
   seccionActual: string = 'Jamón Ibérico De Bellota Y Corte';
   top: any = '';
   visionHi = false;
+  primeraVez = false;
 
   constructor(private comidasServices: ComidasService) {}
 
@@ -20,7 +21,6 @@ export class CartaComponent implements OnInit {
     this.arrComidas = this.comidasServices.getAll(
       'Jamón Ibérico De Bellota Y Corte'
     );
-    console.log(this.arrComidas);
     this.arrSecciones = this.comidasServices.getAll();
     this.arrSecciones = this.arrSecciones.reduce((acc, item) => {
       if (!acc.includes(item)) {
@@ -50,27 +50,34 @@ export class CartaComponent implements OnInit {
   getSection() {}
 
   posicion() {
-    navigator.geolocation.watchPosition(
-      (pos) => {
-        let { coords: { latitude, longitude } = {} } = pos;
+    if (this.primeraVez === false) {
+      navigator.geolocation.watchPosition(
+        (pos) => {
+          let { coords: { latitude, longitude } = {} } = pos;
 
-        //if (latitude == 36.1618232 && longitude == -5.3518956) {
-        // 36.16160 36.16200 -5.35170 -5.35250
-        if (
-          latitude! > 36.1616 &&
-          latitude! < 36.162 &&
-          longitude! < -5.3517 &&
-          longitude! > -5.3525
-        ) {
-          this.visionHi = true;
-        } else {
-          this.visionHi = false;
+          //if (latitude == 36.1618232 && longitude == -5.3518956) {
+          // 36.16160 36.16200 -5.35170 -5.35250
+          if (
+            latitude! > 36.1616 &&
+            latitude! < 36.162 &&
+            longitude! < -5.3517 &&
+            longitude! > -5.3525
+          ) {
+            this.visionHi = true;
+            this.arrComidas = [];
+            this.seccionActual = "Sugerencias Del Día Fuera De Carta";
+            this.primeraVez = true;
+          } else {
+            this.visionHi = false;
+          }
+        },
+        (err) => {
+          console.warn('Error en el localizador', err);
         }
-      },
-      (err) => {
-        console.warn('Error en el localizador', err);
-      },
-      { timeout: 30, maximumAge: 60 }
-    );
+      );
+    } else {
+      this.arrComidas = [];
+      this.seccionActual = 'Sugerencias Del Día Fuera De Carta';
+    }
   }
 }
